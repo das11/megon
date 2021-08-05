c54da43772caf621c89e5b3231c8a2df26a69040
Kabir Das <interfacev2@Kabirs-Interfacev2.local>
2021-08-05 12:19:42 +0530

    homepage

---
+++ wire1/wp-content/themes/dgstudio/js/home.js
@@ -0,0 +1,16 @@
+jQuery(document).ready(function(){
+    console.log("yes")
+    // $(".video-play").on('click', function(e) {
+    //     console.log("Clicked PLAY")
+    //     e.preventDefault(); 
+    //     var vidWrap = $(this).parent(),
+    //         iframe = vidWrap.find('.video iframe'),
+    //         iframeSrc = iframe.attr('src'),
+    //         iframePlay = iframeSrc += "?autoplay=1";
+    //     vidWrap.children('.video-thumbnail').fadeOut();
+    //     vidWrap.children('.video-play').fadeOut();
+    //     vidWrap.find('.video iframe').attr('src', iframePlay);
+
+
+    // });
+});
